f76c9e0271110d05d707a7dd3eb6c1d6d54a32a2
spamegg <spamegg@pm.me>
2025-01-20 20:34:36 +0300

    2024 day 25, update Scala 3.6.3

--- project.scala
+++ project.scala
@@ -1,11 +1,11 @@
-//> using scala 3.6.2
+//> using scala 3.6.3
 //> using toolkit 0.6.0
 //> using dep org.apache.commons:commons-digester3:3.2
 //> using dep org.apache.commons:commons-math4-core:4.0-beta1
 //> using dep org.apache.commons:commons-numbers-primes:1.2
 //> using dep com.google.guava:guava:33.1.0-jre
-//> using dep commons-codec:commons-codec:1.17.1
+//> using dep commons-codec:commons-codec:1.17.2
 //> using dep org.scala-graph:graph-core_2.13:2.0.2
-//> using dep org.scala-lang.modules::scala-parallel-collections:1.0.4
+//> using dep org.scala-lang.modules::scala-parallel-collections:1.2.0
 //> using dep org.scala-lang.modules::scala-collection-contrib:0.4.0
 //> using options -experimental -language:experimental.namedTuples
