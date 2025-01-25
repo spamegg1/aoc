73cf34c3d9b4c7a6d189f910c097f3c2bc6b8f6b
spamegg <spamegg@pm.me>
2025-01-25 16:26:08 +0300

    2015 day 19 refactor

--- 2015/19/19.scala
+++ 2015/19/19.scala
@@ -1,75 +1,3 @@
-/*
---- Day 19: Medicine for Rudolph ---
-Rudolph the Red-Nosed Reindeer is sick!
-His nose isn't shining very brightly, and he needs medicine.
-
-Red-Nosed Reindeer biology isn't similar to regular reindeer biology;
-Rudolph is going to need custom-made medicine.
-Unfortunately, Red-Nosed Reindeer chemistry isn't
-similar to regular reindeer chemistry, either.
-
-The North Pole is equipped with a Red-Nosed Reindeer nuclear fusion/fission plant,
-capable of constructing any Red-Nosed Reindeer molecule you need.
-It works by starting with some input molecule and then doing
-a series of replacements, one per step, until it has the right molecule.
-
-However, the machine has to be calibrated before it can be used.
-Calibration involves determining the number of molecules that can
-be generated in one step from a given starting point.
-
-For example, imagine a simpler machine that supports only the following replacements:
-
-H => HO
-H => OH
-O => HH
-
-Given the replacements above and starting with HOH,
-the following molecules could be generated:
-  HOOH (via H => HO on the first H).
-  HOHO (via H => HO on the second H).
-  OHOH (via H => OH on the first H).
-  HOOH (via H => OH on the second H).
-  HHHH (via O => HH).
-
-So, in the example above, there are 4 distinct molecules
-(not five, because HOOH appears twice) after one replacement from HOH.
-Santa's favorite molecule, HOHOHO, can become 7 distinct molecules
-(over nine replacements: six from H, and three from O).
-
-The machine replaces without regard for the surrounding characters.
-For example, given the string H2O, the transition H => OO would result in OO2O.
-
-Your puzzle input describes all of the possible replacements and,
-at the bottom, the medicine molecule for which you need to calibrate
-the machine. How many distinct molecules can be created after all
-the different ways you can do one replacement on the medicine molecule?
-
---- Part Two ---
-Now that the machine is calibrated, you're ready to begin molecule fabrication.
-
-Molecule fabrication always begins with just a single electron, e,
-and applying replacements one at a time, just like the ones during calibration.
-
-For example, suppose you have the following replacements:
-
-e => H
-e => O
-H => HO
-H => OH
-O => HH
-
-If you'd like to make HOH, you start with e, and then make the following replacements:
-  e => O to get O
-  O => HH to get HH
-  H => OH (on the second H) to get HOH
-
-So, you could make HOH after 3 steps.
-Santa's favorite molecule, HOHOHO, can be made in 6 steps.
-
-How long will it take to make the medicine?
-Given the available replacements and the medicine molecule in your puzzle input,
-what is the fewest number of steps to go from e to the medicine molecule?
- */
 package aoc2015.day19
 
 import util.matching.Regex, Regex.Match
@@ -116,22 +44,22 @@ object Solving:
     steps
 
 object Test:
-  lazy val file    = os.pwd / "2015" / "19" / "19.test.input.txt"
-  lazy val lines   = os.read.lines(file)
-  lazy val result1 = Solving.solve1(lines)("HOHOHO") // 7
-  lazy val result2 = Solving.solve2(lines)("HOHOHO") // 6
+  lazy val file  = os.pwd / "2015" / "19" / "19.test.input.txt"
+  lazy val lines = os.read.lines(file)
+  lazy val res1  = Solving.solve1(lines)("HOHOHO") // 7
+  lazy val res2  = Solving.solve2(lines)("HOHOHO") // 6
 
 object Main:
-  lazy val file1   = os.pwd / "2015" / "19" / "19.input.txt"
-  lazy val file2   = os.pwd / "2015" / "19" / "19.input.2.txt"
-  lazy val lines   = os.read.lines(file1)
-  lazy val line    = os.read.lines(file2).head
-  lazy val result1 = Solving.solve1(lines)(line) // 535
-  lazy val result2 = Solving.solve2(lines)(line) //
+  lazy val file1 = os.pwd / "2015" / "19" / "19.input.txt"
+  lazy val file2 = os.pwd / "2015" / "19" / "19.input.2.txt"
+  lazy val lines = os.read.lines(file1)
+  lazy val line  = os.read.lines(file2).head
+  lazy val res1  = Solving.solve1(lines)(line)
+  lazy val res2  = Solving.solve2(lines)(line)
 
 @main
 def run =
-  println(Test.result1) // part 1: 7
-  // println(Test.result2) // part 2: 6
-  println(Main.result1) // part 1: 535
-  // println(Main.result2) // part 2:
+  println(Test.res1) // part 1: 7
+  // println(Test.res2) // part 2: 6
+  println(Main.res1) // part 1: 535
+  // println(Main.res2) // part 2:
