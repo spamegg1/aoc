f76f71c326c5516ce92ca3ff6aca0dc0da01b960
spamegg <spamegg@pm.me>
2025-01-26 18:10:24 +0300

    2015 day 24

--- 2015/24/24.scala
+++ 2015/24/24.scala
@@ -1,59 +1,36 @@
 package aoc2015.day24
 
 object DataDefs:
-  enum Tree:
-    case Leaf(set: Set[Int])
-    case Half(right: Tree)
-    case Fork(left: Tree, right: Tree)
-
-    def allLeaves: List[Set[Int]] = this match
-      case Leaf(set)         => List(set)
-      case Half(right)       => right.allLeaves
-      case Fork(left, right) => left.allLeaves ::: right.allLeaves
+  val MAX = 6 // I will only consider subsets up to this size
 
 object Parsing:
-  def parse(lines: Seq[String]) = lines.map(_.toInt).toList.reverse
+  def parse(lines: Seq[String]) = lines.map(_.toLong)
 
 object Solving:
-  import DataDefs.*, Tree.*
+  import DataDefs.*
 
-  def add(total: Int, head: Int, tree: Tree): Tree = tree match
-    case Leaf(set) =>
-      if set.sum == total then tree
-      else if set.sum + head <= total then Fork(Leaf(set.incl(head)), tree)
-      else Half(tree)
-    case Half(right)       => Half(add(total, head, right))
-    case Fork(left, right) => Fork(add(total, head, left), add(total, head, right))
+  def solve(factor: Long)(lines: Seq[String]) =
+    val nums  = Parsing.parse(lines)
+    val third = nums.sum / factor
+    (1 to MAX) // get all subsets of size 1, ..., MAX
+      .flatMap: size =>
+        nums                      // Seq[Long]
+          .combinations(size)     // Iterator[Seq[Long]]
+          .filter(_.sum == third) // Iterator[Seq[Long]]
+          .toSeq                  // Seq[Seq[Long]]
+      .groupBy(_.size)            // Map[Int, Seq[Seq[Long]]]
+      .minBy(_._1)                // (Int, Seq[Seq[Long]])
+      ._2                         // Seq[Seq[Long]]
+      .minBy(_.product)           // Seq[Long]
+      .product                    // Long
 
-  @annotation.tailrec
-  def makeTree(total: Int, nums: List[Int], acc: Tree): Tree = nums match
-    case head :: next => makeTree(total, next, add(total, head, acc))
-    case Nil          => acc
-
-  def sets(nums: List[Int]): List[Set[Int]] =
-    val total = nums.sum / 3
-    val tree  = makeTree(total, nums, Leaf(Set()))
-    tree.allLeaves.filter(_.sum == total)
-
-  def partitions(nums: Set[Int], sets: List[Set[Int]]) = sets
-    .combinations(3)
-    .filter(combo => combo.tail.foldLeft(combo.head)(_ union _) == nums)
-    .map(_.sortBy(_.size))
-    .toList
-
-  def solve1(lines: Seq[String]) =
-    val nums        = Parsing.parse(lines) // descending
-    val triples     = partitions(nums.toSet, sets(nums))
-    val fewest      = triples.minBy(_.head.size).head.size
-    val mostLegroom = triples.filter(_.head.size == fewest)
-    mostLegroom.minBy(_.head.product).head.product
-
-  def solve2(lines: Seq[String]) = 0L
+  val solve1 = solve(3L)
+  val solve2 = solve(4L)
 
 object Test:
   lazy val file  = os.pwd / "2015" / "24" / "24.test.input.txt"
   lazy val lines = os.read.lines(file)
-  lazy val res1  = Solving.solve1(lines) // 99
+  lazy val res1  = Solving.solve1(lines)
   lazy val res2  = Solving.solve2(lines)
 
 object Main:
@@ -64,7 +41,7 @@ object Main:
 
 @main
 def run: Unit =
-  println(Test.res1)
-  println(Test.res2)
-  println(Main.res1)
-  println(Main.res2)
+  println(Test.res1) // part 1: 99
+  println(Test.res2) // part 2: 44
+  println(Main.res1) // part 1: 10439961859
+  println(Main.res2) // part 2: 72050269
