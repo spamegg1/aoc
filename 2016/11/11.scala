6f7bc5b3f10a91e8660819b883dedc2dcdadb43c
spamegg <spamegg@pm.me>
2025-01-30 18:15:04 +0300

    2016 day 11 - finally a really clean solution!

--- 2016/11/11.scala
+++ 2016/11/11.scala
@@ -1,109 +1,103 @@
 package aoc2016.day11
 
 object DataDefs:
-  enum Level:
-    case One, Two, Three, Four
-    def neighbors = this match
-      case One   => Set(Two)
-      case Two   => Set(One, Three)
-      case Three => Set(Two, Four)
-      case Four  => Set(Three)
-  import Level.*
+  type Isotope  = String // plutonium, etc.
+  type Device   = String // microchip, generator
+  type Thing    = (iso: Isotope, dev: Device)
+  type Floor    = Seq[Thing]
+  type Counter  = Int
+  type Elevator = Int
+  type State    = (counter: Counter, elevator: Elevator, floors: Seq[Floor])
+  type Record   = (elevator: Elevator, floorFreqs: Seq[Map[Device, Counter]])
 
-  enum Kind:
-    case Chip, RTG
-  import Kind.*
+  extension (floor: Floor)
+    def hasOneDeviceType = floor.map(_.dev).distinct.size < 2
+    def isPairedSafely = floor
+      .filter(_.dev == "microchip")
+      .map(_.iso)
+      .forall(isotope => floor.contains((iso = isotope, dev = "generator")))
+    def isSafe = floor.hasOneDeviceType || floor.isPairedSafely
+    def freq   = floor.map(_.dev).groupMapReduce(identity)(_ => 1)(_ + _)
 
-  enum Element:
-    case Promethium, Cobalt, Curium, Ruthenium, Plutonium, Hydrogen, Lithium
+  extension (floors: Seq[Floor])
+    def areDone = floors(0).isEmpty && floors(1).isEmpty && floors(2).isEmpty
+    def freqs   = floors.map(_.freq)
 
-  case class Item(kind: Kind, elt: Element)
+  extension (s: State)
+    def curr   = s.floors(s.elevator)
+    def moves1 = s.curr.toSeq.combinations(1)
+    def moves2 = s.curr.toSeq.combinations(2)
+    def moves  = s.moves1 ++ s.moves2
+    def levels = Seq(s.elevator - 1, s.elevator + 1).filter(_ < 4).filter(0 <= _)
+    def states =
+      for
+        move  <- s.moves
+        level <- s.levels
+        floor  = s.floors(level)   // next floor we move into
+        newCur = s.curr.diff(move) // remove things from current floor
+        newFlr = floor.union(move) // add things to next floor
+        if newCur.isSafe && newFlr.isSafe
+        newFloors = s.floors
+          .updated(s.elevator, newCur) // remove things from current floor
+          .updated(level, newFlr)      // add things to next floor
+      yield (counter = s.counter + 1, elevator = level, floors = newFloors)
+    def record = (elevator = s.elevator, floorFreqs = s.floors.freqs)
 
-  case class Box(level: Level, items: Set[Item]): // both elevator and floor are boxes
-    def size                         = items.size
-    def isOperational                = 1 <= size && size <= 2 // for elevator
-    def changeLevel(newLevel: Level) = copy(level = newLevel)
-    def take(item: Item)             = copy(items = items + item)
-    def drop(item: Item)             = copy(items = items - item)
-    def safeToGiveTo(that: Box)      = items.filter(that.take(_).isSafe)
-    def safeToDrop                   = items.filter(drop(_).isSafe)
+  object State:
+    def apply(floors: Seq[Floor]): State = (counter = 0, elevator = 0, floors = floors)
 
-    def chips      = items.filter(_.kind == Chip)
-    def generators = items.filter(_.kind == RTG)
-    def isSafe: Boolean = generators.isEmpty || chips.forall: chip =>
-      generators.exists(_.kind == chip.kind)
-
-    def union(that: Box)    = Box(level, items.union(that.items)) // assume same level
-    def isSafeOn(that: Box) = union(that).isSafe
-
-  type Floor    = Box
-  type Elevator = Box
-
-  case class State(floors: Map[Level, Floor], elevator: Elevator):
-    def currentLevel = elevator.level
-    def currentFloor = floors(currentLevel)
-    def isWon        = floors.forall((lvl, floor) => lvl == Four || floor.items.isEmpty)
-
-    def nextStates: Set[State] =
-      // first look at which chips / generators can be picked from / dropped to floor
-      val possibleTakesDrops =
-        if elevator.size == 1 then // elevator can carry one more item
-          currentFloor.safeToDrop.map: item =>
-            (elevator.take(item), currentFloor.drop(item))
-        else if elevator.size == 2 then // elevator can drop one item to floor
-          elevator
-            .safeToGiveTo(currentFloor)
-            .map(item => (elevator.drop(item), currentFloor.take(item)))
-        else Set()
-
-      val possibleElevatorsFloors = possibleTakesDrops + ((elevator, currentFloor))
-
-      // then consider neighbors of current level
-      val neighborFloors = elevator.level.neighbors.map(level => level -> floors(level))
-      val possibleMoves =
-        for
-          (neighborLevel, neighborFloor)   <- neighborFloors
-          (nextElevator, nextCurrentFloor) <- possibleElevatorsFloors
-          movedElevator = nextElevator.changeLevel(neighborLevel)
-          if movedElevator.isSafeOn(neighborFloor)
-        yield (movedElevator, nextCurrentFloor)
-
-      possibleMoves.map: (movedElevator, nextCurrentFloor) =>
-        State(floors.updated(currentLevel, nextCurrentFloor), movedElevator)
-
-object Solving:
+object Parsing:
   import DataDefs.*
 
-  def solve1(start: Set[State]) =
-    var steps   = 0
-    val queue   = collection.mutable.Queue.from(start)
-    var visited = Set[State]() // avoid going back and forth forever
-    var ress    = List[(Int, State)]()
+  val regex = """(\w+)(?:-compatible)? (microchip|generator)""".r
 
-    while queue.nonEmpty do
-      val state = queue.dequeue()
-      if state.isWon then ress ::= (steps, state)
-      else
-        visited += state
-        queue.enqueueAll(state.nextStates.filterNot(visited.contains))
-      steps += 1
-    ress
+  def parseLine(line: String): Floor = regex
+    .findAllMatchIn(line)
+    .map(mat => (iso = mat.group(1), dev = mat.group(2)))
+    .toSeq
 
-  def solve2(start: Set[State]) = 0L
+  def parse(lines: Seq[String]): Seq[Floor] = lines map parseLine
+
+object Solving:
+  import DataDefs.*, util.boundary, boundary.break, collection.mutable.Queue
+
+  def solve(lines: Seq[String]) =
+    val floors  = Parsing.parse(lines)
+    val start   = State(floors)
+    var visited = Set(start.record)
+    val queue   = Queue(start)
+    var result  = 0
+
+    boundary:
+      while queue.nonEmpty do
+        val state = queue.dequeue()
+        if state.floors.areDone then
+          result = state.counter
+          break()
+        for
+          nextState <- state.states
+          record = nextState.record
+        do
+          if !visited.contains(record) then
+            visited += record
+            queue.enqueue(nextState)
+    result
 
 object Test:
-  lazy val start = TestStates.testStates
-  lazy val res1  = Solving.solve1(start) // part 1: 11
-  lazy val res2  = Solving.solve2(start)
+  lazy val file  = os.pwd / "2016" / "11" / "11.test.input.txt"
+  lazy val lines = os.read.lines(file)
+  lazy val res   = Solving.solve(lines)
 
 object Main:
-  lazy val start = MainStates.mainStates
-  lazy val res1  = Solving.solve1(start) // part 1: ???
-  lazy val res2  = Solving.solve2(start)
+  lazy val file1  = os.pwd / "2016" / "11" / "11.input.txt"
+  lazy val file2  = os.pwd / "2016" / "11" / "11.input.2.txt"
+  lazy val lines1 = os.read.lines(file1)
+  lazy val lines2 = os.read.lines(file2)
+  lazy val res1   = Solving.solve(lines1)
+  lazy val res2   = Solving.solve(lines2)
 
 @main
 def run: Unit =
-  println(Test.res1) // part 1: 11
-  // println(Test.res2) // part 2:
-  // println(Main.res1) // part 1: ???
-  // println(Main.res2) // part 2: ???
+  println(Test.res)  // part 1: 11
+  println(Main.res1) // part 1: 33
+  println(Main.res2) // part 2: 57
