4945290b5de48fe85dfa38240b3696403cd4109f
spamegg <spamegg@pm.me>
2025-02-23 18:53:19 +0300

    2018 day 25

--- 2018/25/25.scala
+++ 2018/25/25.scala
@@ -1,15 +1,28 @@
 package aoc2018.day25
 
 object DataDefs:
-  ???
+  type Pos = (x: Int, y: Int, z: Int, w: Int)
+  extension (p: Pos)
+    def dist(that: Pos): Int =
+      (p.x - that.x).abs + (p.y - that.y).abs + (p.z - that.z).abs + (p.w - that.w).abs
 
 object Parsing:
   import DataDefs.*
-  def parse(lines: Seq[String]) = ???
+
+  def parseLine(line: String): Pos = line match
+    case s"$x,$y,$z,$w" => (x.toInt, y.toInt, z.toInt, w.toInt)
+
+  def parse(lines: Seq[String]) = lines map parseLine
 
 object Solving:
   import DataDefs.*
-  def solve(lines: Seq[String]) = 0L
+
+  def solve(lines: Seq[String]) = Parsing
+    .parse(lines)
+    .foldLeft(Set.empty[Set[Pos]]): (constellations, pos) =>
+      val (near, far) = constellations.partition(_.exists(_.dist(pos) <= 3))
+      far + (near.flatten + pos)
+    .size
 
 object Test:
   lazy val file1 = os.pwd / "2018" / "25" / "25.test.input.1.txt"
