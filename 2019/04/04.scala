6b060a1e8f65be74218793b546778edfc93e7377
spamegg <spamegg@pm.me>
2025-02-23 19:45:48 +0300

    preparing 2019

---
+++ 2019/04/04.scala
@@ -0,0 +1,75 @@
+/*
+--- Day 4: Secure Container ---
+You arrive at the Venus fuel depot only to discover it's protected by a password.
+The Elves had written the password on a sticky note, but someone threw it out.
+
+However, they do remember a few key facts about the password:
+  It is a six-digit number.
+  The value is within the range given in your puzzle input.
+  Two adjacent digits are the same (like 22 in 122345).
+  Going from left to right, the digits never decrease;
+  they only ever increase or stay the same (like 111123 or 135679).
+
+Other than the range rule, the following are true:
+  111111 meets these criteria (double 11, never decreases).
+  223450 does not meet these criteria (decreasing pair of digits 50).
+  123789 does not meet these criteria (no double).
+
+How many different passwords within the range given in your puzzle input
+meet these criteria? Your puzzle input is 156218-652527.
+
+--- Part Two ---
+An Elf just remembered one more important detail:
+the two adjacent matching digits are not part of a larger group of matching digits.
+
+Given this additional criterion, but still ignoring the range rule,
+the following are now true:
+  112233 meets these criteria because the digits never decrease and
+    all repeated digits are exactly two digits long.
+  123444 no longer meets the criteria (the repeated 44 is part of a larger group of 444).
+  111122 meets the criteria
+    (even though 1 is repeated more than twice, it still contains a double 22).
+
+How many different passwords within the range given
+in your puzzle input meet all of the criteria?
+ */
+package aoc2019.day04
+
+object DataDefs:
+  ???
+
+object Parsing:
+  import DataDefs.*
+  ???
+
+object Solving:
+  import DataDefs.*
+  def solve1(lines: Seq[String]) =
+    (156218 to 652527).view
+      .map(_.toString)
+      .filter(str => (0 to 4).forall(index => str(index) <= str(index + 1)))
+      .filter(str => (0 to 4).exists(index => str(index) == str(index + 1)))
+      .size
+
+  def solve2(lines: Seq[String]) =
+    (156218 to 652527).view
+      .map(_.toString)
+      .filter(str => (0 to 4).forall(index => str(index) <= str(index + 1)))
+      .filter: str =>
+        (0 to 4).exists: index =>
+          str(index) == str(index + 1) && str.count(_ == str(index)) == 2
+      .size
+
+object Test:
+  private lazy val lines = os.read.lines(os.pwd / "2019" / "04" / "04.test.input.txt")
+  lazy val res1          = Solving.solve1(lines)
+  lazy val res2          = Solving.solve2(lines)
+// Test.res1 // part 1:
+// Test.res2 // part 2:
+
+object Main:
+  lazy val lines = os.read.lines(os.pwd / "2019" / "04" / "04.input.txt")
+  lazy val res1  = Solving.solve1(lines)
+  lazy val res2  = Solving.solve2(lines)
+// Main.res1 // part 1: 1694
+// Main.res2 // part 2: 1148
---
+++ 2019/18/18.scala
@@ -0,0 +1,152 @@
+/*
+--- Day 18: Many-Worlds Interpretation ---
+As you approach Neptune, a planetary security system detects you and
+activates a giant tractor beam on Triton! You have no choice but to land.
+
+A scan of the local area reveals only one interesting feature: a massive
+underground vault. You generate a map of the tunnels (your puzzle input).
+The tunnels are too narrow to move diagonally.
+
+Only one entrance (marked @) is present among the open passages (marked .)
+and stone walls (#), but you also detect an assortment of keys
+(shown as lowercase letters) and doors (shown as uppercase letters).
+Keys of a given letter open the door of the same letter:
+a opens A, b opens B, and so on.
+You aren't sure which key you need to disable the tractor beam,
+so you'll need to collect all of them.
+
+For example, suppose you have the following map:
+
+#########
+#b.A.@.a#
+#########
+
+Starting from the entrance (@), you can only access a large door
+(A) and a key (a). Moving toward the door doesn't help you,
+but you can move 2 steps to collect the key, unlocking A in the process:
+
+#########
+#b.....@#
+#########
+
+Then, you can move 6 steps to collect the only other key, b:
+
+#########
+#@......#
+#########
+
+So, collecting every key took a total of 8 steps.
+
+Here is a larger example:
+
+########################
+#f.D.E.e.C.b.A.@.a.B.c.#
+######################.#
+#d.....................#
+########################
+
+The only reasonable move is to take key a and unlock door A:
+
+########################
+#f.D.E.e.C.b.....@.B.c.#
+######################.#
+#d.....................#
+########################
+
+Then, do the same with key b:
+
+########################
+#f.D.E.e.C.@.........c.#
+######################.#
+#d.....................#
+########################
+
+...and the same with key c:
+
+########################
+#f.D.E.e.............@.#
+######################.#
+#d.....................#
+########################
+
+Now, you have a choice between keys d and e. While key e is closer,
+collecting it now would be slower in the long run than collecting
+key d first, so that's the best choice:
+
+########################
+#f...E.e...............#
+######################.#
+#@.....................#
+########################
+
+Finally, collect key e to unlock door E, then collect key f,
+taking a grand total of 86 steps.
+
+Here are a few more examples:
+
+########################
+#...............b.C.D.f#
+#.######################
+#.....@.a.B.c.d.A.e.F.g#
+########################
+
+Shortest path is 132 steps: b, a, c, d, f, e, g
+
+#################
+#i.G..c...e..H.p#
+########.########
+#j.A..b...f..D.o#
+########@########
+#k.E..a...g..B.n#
+########.########
+#l.F..d...h..C.m#
+#################
+
+Shortest paths are 136 steps;
+one is: a, f, b, j, g, n, h, d, l, o, e, p, c, i, k, m
+
+########################
+#@..............ac.GI.b#
+###d#e#f################
+###A#B#C################
+###g#h#i################
+########################
+
+Shortest paths are 81 steps; one is: a, c, f, i, d, g, b, e, h
+
+How many steps is the shortest path that collects all of the keys?
+
+ */
+package aoc2019.day18
+
+object DataDefs:
+  ???
+
+object Parsing:
+  import DataDefs.*
+  def parseLine(line: String)   = ???
+  def parse(lines: Seq[String]) = lines map parseLine
+
+object Solving:
+  import DataDefs.*
+  def solve1(lines: Seq[String]) = 0L
+  def solve2(lines: Seq[String]) = 0L
+
+object Test:
+  private lazy val lines1 = os.read.lines(os.pwd / "2019" / "18" / "18.test.input.txt")
+  private lazy val lines2 = os.read.lines(os.pwd / "2019" / "18" / "18.test.input.2.txt")
+  private lazy val lines3 = os.read.lines(os.pwd / "2019" / "18" / "18.test.input.3.txt")
+  private lazy val lines4 = os.read.lines(os.pwd / "2019" / "18" / "18.test.input.4.txt")
+  private lazy val lines5 = os.read.lines(os.pwd / "2019" / "18" / "18.test.input.5.txt")
+  private lazy val lines  = List(lines1, lines2, lines3, lines4, lines5)
+  lazy val res1           = lines map Solving.solve1
+  lazy val res2           = lines map Solving.solve2
+// Test.res1 // part 1: 8,86,132,136,81
+// Test.res2 // part 2:
+
+object Main:
+  lazy val lines = os.read.lines(os.pwd / "2019" / "18" / "18.input.txt")
+  lazy val res1  = Solving.solve1(lines)
+  lazy val res2  = Solving.solve2(lines)
+// Main.res1 // part 1:
+// Main.res2 // part 2:
