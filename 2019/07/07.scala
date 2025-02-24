6114f329a086bf69e19beee1995b3f21ecd4ea35
spamegg <spamegg@pm.me>
2025-02-24 20:43:59 +0300

    2019 day 07

--- 2019/07/07.scala
+++ 2019/07/07.scala
@@ -1,109 +1,93 @@
-/*
---- Day 7: Amplification Circuit ---
-Based on the navigational maps, you're going to need to send more power
-to your ship's thrusters to reach Santa in time.
-To do this, you'll need to configure a series of amplifiers already installed on the ship.
-
-There are five amplifiers connected in series;
-each one receives an input signal and produces an output signal.
-They are connected such that the first amplifier's output leads
-to the second amplifier's input, the second amplifier's output leads
-to the third amplifier's input, and so on. The first amplifier's
-input value is 0, and the last amplifier's output leads to your ship's thrusters.
-
-    O-------O  O-------O  O-------O  O-------O  O-------O
-0 ->| Amp A |->| Amp B |->| Amp C |->| Amp D |->| Amp E |-> (to thrusters)
-    O-------O  O-------O  O-------O  O-------O  O-------O
-
-The Elves have sent you some Amplifier Controller Software (your puzzle input),
-a program that should run on your existing Intcode computer.
-Each amplifier will need to run a copy of the program.
-
-When a copy of the program starts running on an amplifier,
-it will first use an input instruction to ask the amplifier
-for its current phase setting (an integer from 0 to 4).
-Each phase setting is used exactly once, but the Elves can't
-remember which amplifier needs which phase setting.
-
-The program will then call another input instruction to get the
-amplifier's input signal, compute the correct output signal,
-and supply it back to the amplifier with an output instruction.
-(If the amplifier has not yet received an input signal, it waits until one arrives.)
-
-Your job is to find the largest output signal that can be sent
-to the thrusters by trying every possible combination of phase
-settings on the amplifiers. Make sure that memory is not shared
-or reused between copies of the program.
-
-For example, suppose you want to try the phase setting sequence 3,1,2,4,0,
-which would mean setting amplifier A to phase setting 3,
-amplifier B to setting 1, C to 2, D to 4, and E to 0.
-Then, you could determine the output signal that gets sent from amplifier E
-to the thrusters with the following steps:
-  Start the copy of the amplifier controller software that will run on amplifier A.
-    At its first input instruction, provide it the amplifier's phase setting, 3.
-    At its second input instruction, provide it the input signal, 0.
-    After some calculations, it will use an output instruction to indicate
-    the amplifier's output signal.
-  Start the software for amplifier B.
-    Provide it the phase setting (1) and then whatever output signal was
-      produced from amplifier A.
-    It will then produce a new output signal destined for amplifier C.
-  Start the software for amplifier C,
-    provide the phase setting (2) and the value from amplifier B,
-    then collect its output signal.
-  Run amplifier D's software, provide the phase setting (4) and input value,
-    and collect its output signal.
-  Run amplifier E's software, provide the phase setting (0) and input value,
-    and collect its output signal.
-
-The final output signal from amplifier E would be sent to the thrusters.
-However, this phase setting sequence may not have been the best one;
-another sequence might have sent a higher signal to the thrusters.
-
-Here are some example programs:
-  Max thruster signal 43210 (from phase setting sequence 4,3,2,1,0):
-    3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0
-  Max thruster signal 54321 (from phase setting sequence 0,1,2,3,4):
-    3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0
-  Max thruster signal 65210 (from phase setting sequence 1,0,4,3,2):
-    3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,
-    33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0
-
-Try every combination of phase settings on the amplifiers.
-What is the highest signal that can be sent to the thrusters?
-
- */
 package aoc2019.day07
 
 object DataDefs:
-  ???
+  val powers = Map(1 -> 100, 2 -> 1000)
+
+  enum State:
+    case Initial, Running, Halted
+    case Output(value: Int)
+  import State.*
+
+  case class Cpu(ip: Int, code: Seq[Int], in: Seq[Int], state: State):
+    def next = code(ip) % 100 match
+      case 1 => Cpu(ip + 4, write(3, read(1) + read(2)), in, Running) // Add
+      case 2 => Cpu(ip + 4, write(3, read(1) * read(2)), in, Running) // Multiply
+      case 3 => Cpu(ip + 2, write(1, in.head), in.tail, Running)      // Read
+      case 4 => Cpu(ip + 2, code, in, Output(read(1)))                // Write
+      case 5  => Cpu(if read(1) != 0 then read(2) else ip + 3, code, in, Running) // jt
+      case 6  => Cpu(if read(1) == 0 then read(2) else ip + 3, code, in, Running) // jf
+      case 7  => Cpu(ip + 4, write(3, if read(1) < read(2) then 1 else 0), in, Running)
+      case 8  => Cpu(ip + 4, write(3, if read(1) == read(2) then 1 else 0), in, Running)
+      case 99 => Cpu(ip, code, in, Halted)                                        // Halt
+
+    private def read(offset: Int): Int = (code(ip) / powers(offset)) % 10 match
+      case 0 => code(code(ip + offset))
+      case 1 => code(ip + offset)
+
+    def write(offset: Int, value: Int) = code.updated(code(ip + offset), value)
+    def withIn(next: Int*): Cpu        = copy(in = in.appendedAll(next))
+    def nextOut: Cpu = Iterator.iterate(next)(_.next).dropWhile(_.state == Running).next()
+
+    def allOut: Seq[Int] =
+      val output = Iterator.iterate(this)(_.nextOut).takeWhile(_.state != Halted)
+      output.toSeq.map(_.state).collect { case Output(value) => value }
+  end Cpu
+
+  object Cpu:
+    def apply(code: Seq[Int]) = new Cpu(0, code, Seq(), Initial)
 
 object Parsing:
-  import DataDefs.*
-  ???
+  def parse(line: String) = line.split(",").map(_.toInt).toSeq
 
 object Solving:
-  import DataDefs.*
-  def solve1(line: String) = 0L
-  def solve2(line: String) = 0L
+  import DataDefs.*, State.*
+
+  def solve1(line: String) =
+    val code = Parsing.parse(line)
+    (0 to 4).permutations
+      .map: perm =>
+        perm
+          .foldLeft(0): (total, next) =>
+            Cpu(code)
+              .withIn(next, total)
+              .allOut
+              .last
+      .max
+
+  @annotation.tailrec
+  def helper(prevOut: Int)(amps: Seq[Cpu]): Int =
+    val nextAmp = amps.head.withIn(prevOut).nextOut
+    nextAmp.state match
+      case Output(nextOut) => helper(nextOut)(amps.tail :+ nextAmp)
+      case _               => prevOut
+
+  def solve2(line: String) =
+    val code = Parsing.parse(line)
+    (5 to 9)
+      .map: phase =>
+        Cpu(code).withIn(phase)
+      .permutations
+      .map(helper(0))
+      .max
 
 object Test:
-  private lazy val line1 = os.read.lines(os.pwd / "2019" / "07" / "07.test.1.txt").head
-  private lazy val line2 = os.read.lines(os.pwd / "2019" / "07" / "07.test.2.txt").head
-  private lazy val line3 = os.read.lines(os.pwd / "2019" / "07" / "07.test.3.txt").head
-  lazy val res11         = Solving.solve1(line1)
-  lazy val res12         = Solving.solve1(line2)
-  lazy val res13         = Solving.solve1(line3)
-  lazy val res2          = Solving.solve2(line1)
-// Test.res11 // part 1: 43210 4,3,2,1,0
-// Test.res12 // part 1: 54321 0,1,2,3,4
-// Test.res13 // part 1: 65210 1,0,4,3,2
-// Test.res2 // part 2:
+  lazy val file1 = os.pwd / "2019" / "07" / "07.test.1.txt"
+  lazy val file2 = os.pwd / "2019" / "07" / "07.test.2.txt"
+  lazy val file3 = os.pwd / "2019" / "07" / "07.test.3.txt"
+  lazy val files = Seq(file1, file2, file3)
+  lazy val lines = files.map(os.read.lines(_).head)
+  lazy val res1  = lines map Solving.solve1
+  lazy val res2  = Solving.solve2(lines.head)
 
 object Main:
-  lazy val line = os.read.lines(os.pwd / "2019" / "07" / "07.input.txt").head
+  lazy val file = os.pwd / "2019" / "07" / "07.input.txt"
+  lazy val line = os.read.lines(file).head
   lazy val res1 = Solving.solve1(line)
   lazy val res2 = Solving.solve2(line)
-// Main.res1 // part 1:
-// Main.res2 // part 2:
+
+@main
+def run: Unit =
+  println(Test.res1) // part 1: 43210,54321,65210
+  println(Test.res2) // part 2: 98765
+  println(Main.res1) // part 1: 24625
+  println(Main.res2) // part 2: 36497698
