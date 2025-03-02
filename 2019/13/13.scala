f3782d6726640cefb7500df16162638303c443f4
spamegg <spamegg@pm.me>
2025-03-02 13:22:12 +0300

    2019 day 13

--- 2019/13/13.scala
+++ 2019/13/13.scala
@@ -1,57 +1,77 @@
-/*
---- Day 13: Care Package ---
-As you ponder the solitude of space and the ever-increasing three-hour
-roundtrip for messages between you and Earth, you notice that the Space
-Mail Indicator Light is blinking. To help keep you sane, the Elves have
-sent you a care package.
-
-It's a new game for the ship's arcade cabinet! Unfortunately,
-the arcade is all the way on the other end of the ship.
-Surely, it won't be hard to build your own - the care package even comes with schematics.
-
-The arcade cabinet runs Intcode software like the game the Elves sent (your puzzle input).
-It has a primitive screen capable of drawing square tiles on a grid.
-The software draws tiles to the screen with output instructions:
-every three output instructions specify the x position (distance from the left),
-y position (distance from the top), and tile id. The tile id is interpreted as follows:
-  0 is an empty tile. No game object appears in this tile.
-  1 is a wall tile. Walls are indestructible barriers.
-  2 is a block tile. Blocks can be broken by the ball.
-  3 is a horizontal paddle tile. The paddle is indestructible.
-  4 is a ball tile. The ball moves diagonally and bounces off objects.
-
-For example, a sequence of output values like 1,2,3,6,5,4 would draw a
-horizontal paddle tile (1 tile from the left and 2 tiles from the top)
-and a ball tile (6 tiles from the left and 5 tiles from the top).
-
-Start the game. How many block tiles are on the screen when the game exits?
-
- */
 package aoc2019.day13
 
+import aoc2019.day09.DataDefs.{Cpu, State}
+
 object DataDefs:
-  ???
+  type Pos    = (x: Long, y: Long)
+  type Tiles  = Map[Pos, Long]
+  type Score  = Long
+  type Paddle = Long
+  type Ball   = Long
+  type Output = Seq[Long]
+
+  extension (cpu: Cpu)
+    def raw(amount: Int): Seq[Cpu] = Iterator
+      .iterate(cpu)(_.nextOut)
+      .drop(1)
+      .take(amount)
+      .toSeq
+    def take(amount: Int): (Cpu, Output) =
+      val raw = cpu.raw(amount)
+      val out = raw.map(_.state).collect { case State.Out(value) => value }
+      (raw.last, out)
+
+  case class World(score: Score, paddle: Paddle, ball: Ball, tiles: Tiles):
+    def next(out: Output): World = out
+      .grouped(3)
+      .foldLeft(this):
+        case (world, Seq(x, y, id)) =>
+          val nextScore = if x == -1 then id else world.score
+          val nextPad   = if id == 3 then x else world.paddle
+          val nextBall  = if id == 4 then x else world.ball
+          val nextTiles = world.tiles.updated((x, y), id)
+          World(nextScore, nextPad, nextBall, nextTiles)
+        case _ => this
+
+  case class Game(cpu: Cpu, amount: Int, world: World):
+    def play: Score =
+      val (nextCpu, out) = cpu.take(amount)
+      val nextWorld      = world.next(out)
+      val finalCpu       = nextCpu.withIn((nextWorld.ball - nextWorld.paddle).sign)
+      val nextBlocks     = nextWorld.tiles.values.count(_ == 2)
+      if nextBlocks == 0 then nextWorld.score
+      else Game(finalCpu, 6, nextWorld).play // why 6?
 
 object Parsing:
-  import DataDefs.*
-  def parseLine(line: String)   = ???
-  def parse(lines: Seq[String]) = lines map parseLine
+  def parse(line: String) = line.split(",").map(_.toLong).toSeq
 
 object Solving:
   import DataDefs.*
-  def solve1(line: String) = 0L
-  def solve2(line: String) = 0L
 
-object Test:
-  private lazy val lines = os.read.lines(os.pwd / "2019" / "13" / "13.test.input.txt")
-  lazy val res1          = lines map Solving.solve1
-  lazy val res2          = lines map Solving.solve2
-// Test.res1 // part 1:
-// Test.res2 // part 2:
+  def solve1(line: String) =
+    val mem = Parsing.parse(line)
+    Cpu(mem).allOut
+      .grouped(3)
+      .foldLeft(Map.empty[Pos, Long]) { case (tiles, Seq(x, y, id)) =>
+        tiles.updated((x, y), id)
+      }
+      .values
+      .count(_ == 2)
+
+  def solve2(line: String) =
+    val mem   = Parsing.parse(line)
+    val cpu   = Cpu(mem.updated(0, 2))
+    val world = World(-1, -1, -1, Map())
+    val game  = Game(cpu, 2643, world) // 2643 = (20 * 44 + 1) * 3
+    game.play
 
 object Main:
-  lazy val line = os.read.lines(os.pwd / "2019" / "13" / "13.input.txt").head
+  lazy val file = os.pwd / "2019" / "13" / "13.input.txt"
+  lazy val line = os.read.lines(file).head
   lazy val res1 = Solving.solve1(line)
   lazy val res2 = Solving.solve2(line)
-// Main.res1 // part 1:
-// Main.res2 // part 2:
+
+@main
+def run: Unit =
+  println(Main.res1) // part 1: 277
+  println(Main.res2) // part 2: 12856
