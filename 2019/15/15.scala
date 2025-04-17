06f7feeda16b3676d4dabcdaec4e7c24adb02ef5
spamegg <spamegg@pm.me>
2025-04-17 15:38:47 +0300

    2019 day 15

--- 2019/15/15.scala
+++ 2019/15/15.scala
@@ -1,111 +1,74 @@
-/*
---- Day 15: Oxygen System ---
-Out here in deep space, many things can go wrong.
-Fortunately, many of those things have indicator lights.
-Unfortunately, one of those lights is lit: the oxygen system
-for part of the ship has failed!
-
-According to the readouts, the oxygen system must have failed
-days ago after a rupture in oxygen tank two; that section of
-the ship was automatically sealed once oxygen levels went dangerously low.
-A single remotely-operated repair droid is your only option for fixing the oxygen system.
-
-The Elves' care package included an Intcode program (your puzzle input)
-that you can use to remotely control the repair droid.
-By running that program, you can direct the repair
-droid to the oxygen system and fix the problem.
-
-The remote control program executes the following steps in a loop forever:
-  Accept a movement command via an input instruction.
-  Send the movement command to the repair droid.
-  Wait for the repair droid to finish the movement operation.
-  Report on the status of the repair droid via an output instruction.
-
-Only four movement commands are understood: north (1), south (2), west (3), and east (4).
-Any other command is invalid. The movements differ in direction, but not in distance:
-in a long enough east-west hallway, a series of commands like 4,4,4,4,3,3,3,3 would
-leave the repair droid back where it started.
-
-The repair droid can reply with any of the following status codes:
-  0: The repair droid hit a wall. Its position has not changed.
-  1: The repair droid has moved one step in the requested direction.
-  2: The repair droid has moved one step in the requested direction;
-    its new position is the location of the oxygen system.
-
-You don't know anything about the area around the repair droid,
-but you can figure it out by watching the status codes.
-
-For example, we can draw the area using D for the droid, # for walls,
-. for locations the droid can traverse, and empty space for unexplored locations.
-Then, the initial state looks like this:
-
-   D
-
-To make the droid go north, send it 1.
-If it replies with 0, you know that location is a wall and that the droid didn't move:
-
-   #
-   D
-
-To move east, send 4; a reply of 1 means the movement was successful:
-
-   #
-   .D
-
-Then, perhaps attempts to move north (1), south (2),
-and east (4) are all met with replies of 0:
-
-   ##
-   .D#
-    #
-Now, you know the repair droid is in a dead end. Backtrack with 3
-(which you already know will get a reply of 1
-because you already know that location is open):
-
-   ##
-   D.#
-    #
-
-Then, perhaps west (3) gets a reply of 0, south (2) gets a reply of 1,
-south again (2) gets a reply of 0, and then west (3) gets a reply of 2:
-
-   ##
-  #..#
-  D.#
-   #
-
-Now, because of the reply of 2, you know you've found the oxygen system!
-In this example, it was only 2 moves away from the repair droid's starting position.
-
-What is the fewest number of movement commands required to move the repair droid
-from its starting position to the location of the oxygen system?
-
- */
 package aoc2019.day15
 
+import aoc2019.day09.DataDefs.*
+import collection.mutable.{Map => MMap, PriorityQueue => PQueue}
+
 object DataDefs:
-  ???
+  type Pos   = (x: Int, y: Int)
+  type Cost  = Int
+  type Costs = MMap[Pos, Cost]
+  type Path  = Option[(Pos, Cpu)]
+
+  extension (p: Pos) def +(that: Pos): Pos = (x = p.x + that.x, y = p.y + that.y)
+
+  val neighbors: Seq[(Pos, Int)] = Seq(
+    (x = 0, y = -1) -> 1,
+    (x = 0, y = 1)  -> 2,
+    (x = -1, y = 0) -> 3,
+    (x = 1, y = 0)  -> 4
+  )
 
 object Parsing:
-  import DataDefs.*
-  def parseLine(line: String)   = ???
-  def parse(lines: Seq[String]) = lines map parseLine
+  def parse(line: String) = line.split(",").map(_.toLong).toSeq
 
 object Solving:
-  import DataDefs.*
-  def solve1(line: String) = 0L
-  def solve2(line: String) = 0L
+  import DataDefs.*, State.Out
 
-object Test:
-  private lazy val lines = os.read.lines(os.pwd / "2019" / "15" / "15.test.input.txt")
-  lazy val res1          = lines map Solving.solve1
-  lazy val res2          = lines map Solving.solve2
-// Test.res1 // part 1: 2
-// Test.res2 // part 2:
+  def move(pos: Pos, cpu: Cpu)(delta: Pos, instr: Int): (Pos, Cpu, Long) =
+    val nextPos = pos + delta
+    val nextCpu = cpu.withIn(instr).nextOut
+    val Out(st) = nextCpu.state: @unchecked
+    (nextPos, nextCpu, st)
+
+  def dijkstra(initCpu: Cpu): (Costs, Path) =
+    val start  = (x = 0, y = 0)
+    val costs  = MMap(start -> 0)
+    val order  = Ordering.by[(Pos, Cpu), Int]((pos, _) => costs(pos))
+    val todo   = PQueue(start -> initCpu)(order)
+    var target = Option.empty[(Pos, Cpu)]
+
+    while todo.nonEmpty do
+      val (pos, cpu) = todo.dequeue()
+      neighbors
+        .map(move(pos, cpu))
+        .filter: (nextPos, _, _) =>
+          !costs.contains(nextPos) || costs(pos) + 1 < costs(nextPos)
+        .filter((_, _, status) => 0 < status)
+        .foreach: (nextPos, nextCpu, status) =>
+          costs(nextPos) = costs(pos) + 1
+          todo.enqueue(nextPos -> nextCpu)
+          if status == 2 then target = Some(nextPos -> nextCpu)
+
+    (costs, target)
+
+  def solve1(line: String) =
+    val initCpu               = Cpu(Parsing.parse(line))
+    val (costs, Some(pos, _)) = dijkstra(initCpu): @unchecked
+    costs(pos)
+
+  def solve2(line: String) =
+    val initCpu           = Cpu(Parsing.parse(line))
+    val (_, Some(_, cpu)) = dijkstra(initCpu): @unchecked
+    val (costs, _)        = dijkstra(cpu)
+    costs.values.max
 
 object Main:
-  lazy val line = os.read.lines(os.pwd / "2019" / "15" / "15.input.txt").head
+  lazy val file = os.pwd / "2019" / "15" / "15.input.txt"
+  lazy val line = os.read.lines(file).head
   lazy val res1 = Solving.solve1(line)
   lazy val res2 = Solving.solve2(line)
-// Main.res1 // part 1:
-// Main.res2 // part 2:
+
+@main
+def run: Unit =
+  println(Main.res1) // part 1: 424
+  println(Main.res2) // part 2: 446
